1f083f9f33ad7849ec5622c20a9e10211b2508e7
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2025-11-13 17:32:35 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+1-misol
+a = input(f"ko'cha kirit: ")
+b = input(f"mahalla kirit: ")
+c = input(f"tuman kirit: ")
+d = input(f"viloyat kirit: ")
+kocha = a
+mahalla = b
+tuman = c
+viloyat = d
+print(kocha + " ko'chasi,\n" + mahalla + " mahallasi,\n" + tuman + " tumani,\n" + viloyat + " viloyati")
+print()
+manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+print(manzil)
+print()
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.title())
+print(manzil.capitalize())
